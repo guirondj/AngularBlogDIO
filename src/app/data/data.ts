d0801920ffde60a2461b6c52cffd39d8dcf97cbf
guirondj <lucasguirondj@hotmail.com>
2023-09-04 19:42:47 -0300

    projeto finalizado

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,16 +1,23 @@
 export const data = [
     {
-        "id": 1,
-        "foto": "link",
-        "titulo": "teste conteudo novo",
-        "texto": "ola isso é um teste"
+        "id": "1",
+        "foto": "https://s2-techtudo.glbimg.com/wATd3neKBag-e4TWKaYvvn-h_qw=/0x0:695x401/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/N/3/a5QNA0TG2CyejbEoWnGA/2016-02-01-re-4-amecube.jpg",
+        "titulo": "Resident Evil 4",
+        "texto": "Leon S. Kennedy era apenas um policial novato quando presenciou o tenebroso evento que aconteceu em 1998, na cidade de Raccoon City. Muito tempo se passou e Leon finalmente conseguiu deixar para trás as imagens daquele dia. Seis anos depois, ele é um agente do serviço secreto norte-americano, responsável pela proteção da família presidencial."
     },
 
     {
-        "id": 2,
-        "foto": "link",
-        "titulo": "teste conteudo novo",
-        "texto": "ola isso é um teste"
+        "id": "2",
+        "foto": "https://s2.glbimg.com/-OxBJy8ZDlVopt1Jvh6Y8RRTJO0=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2019/2/J/WlQvthQO2EQtu5oIPYNQ/dciyy7wxoaebwnl.jpg",
+        "titulo": "Campeonato de CS:GO",
+        "texto": "Mas GCzinha, quero jogar um campeonato de CS:GO rapidinho que dê algum dinheirinho. Comofas?"
+    },
+
+    {
+        "id": "3",
+        "foto": "https://www.torredevigilancia.com/wp-content/uploads/2018/12/1473738404_789399_1473744290_noticia_normal-810x456.jpg",
+        "titulo": "Campeonato Mundial de LOL",
+        "texto": "NÚMEROS RECORDES DO CAMPEONATO MUNDIAL DE LOL"
     }
 ];
  
--- src/app/pages/conteudo/conteudo.component.ts
+++ src/app/pages/conteudo/conteudo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import{data} from '../../data/data' 
 
 @Component({
   selector: 'app-conteudo',
@@ -8,9 +9,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ConteudoComponent implements OnInit {
 
-  conteudoFoto:string = "https://s2-techtudo.glbimg.com/wATd3neKBag-e4TWKaYvvn-h_qw=/0x0:695x401/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/N/3/a5QNA0TG2CyejbEoWnGA/2016-02-01-re-4-amecube.jpg"
+  conteudoFoto:string = ""
   conteudoTitulo:string = ""
   conteudoTexto:string = ""
+  private id:string | null = "0"  
 
   constructor(
     private route:ActivatedRoute
@@ -18,8 +20,20 @@ export class ConteudoComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id =value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = data.filter(article => article.id == id)[0]
+
+    this.conteudoTitulo = result.titulo
+    this.conteudoTexto = result.texto
+    this.conteudoFoto = result.foto
+
+    
   }
 
 }
